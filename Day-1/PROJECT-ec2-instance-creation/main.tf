d12b9c625bc997f83714f7101cc2bac11082a53c
pkry921 <126652103+pkry921@users.noreply.github.com>
2024-11-04 05:41:24 +0000

    added subnet and keypair

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-south-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0202d924ec5be94c1"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-0102929c5dcd50329"
+    key_name = "analyticsit"
 }
